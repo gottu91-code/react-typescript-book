e6fedfa48be0aa3f8b55f23b7c2152bee8454a94
gottu91 <gotuu91.afl@gmail.com>
2022-11-25 19:49:09 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import Parent from './components/ContainerSample';
+import Page from './components/ContextSample';
+import Counter from './components/HooksUseState';
+import { Parent } from './components/Parent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Parent /> */}
+      <Page />
+      <hr />
+      <Counter initialValue={0} />
+      <hr />
+      <Parent />
+    </>
   );
 }
 
